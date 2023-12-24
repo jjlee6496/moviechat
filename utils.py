6849741e47cd69dd30c71e28804459102bbab001
jjlee6496 <ljj3499@gmail.com>
2023-12-24 22:22:17 +0900

    Updated utils.py

--- utils.py
+++ utils.py
@@ -4,3 +4,13 @@ from dotenv import load_dotenv, find_dotenv
 def get_openai_api_key():
     _ = load_dotenv(find_dotenv())
     return os.getenv("OPENAI_API_KEY")
+
+# 검색 결과가 달라서 사용 불가
+# def get_naver_api_key():
+#     _ = load_dotenv(find_dotenv())
+#     return os.getenv("NAVER_CLIENT_ID"), os.getenv("NAVER_CLIENT_SECRET")
+
+
+def get_serp_api_key():
+    _ = load_dotenv(find_dotenv())
+    return os.getenv("SERPAPI_KEY")
