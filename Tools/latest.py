eed4afff24477c8864652b0c77b8176db2051d85
jjlee6496 <ljj3499@gmail.com>
2023-12-29 19:03:23 +0900

    context length문제 해결

--- Tools/latest.py
+++ Tools/latest.py
@@ -1,15 +1,24 @@
+# 참고: https://github.com/langchain-ai/langchain/issues/7685
 import pandas as pd
-from langchain.agents import tool
-from pydantic.v1 import BaseModel, Field
+from langchain.tools import BaseTool
+from typing import Union, Dict, Tuple
+class LatestmovieTool(BaseTool):
+    name = "get_latest_movies"
+    description = "Get the current playing movies. No parameter needed from input"
 
-class latest_movie(BaseModel):
-    query: str = Field(..., description= 'User asked for latest movie')
+    def _to_args_and_kwargs(self, tool_input: Union[str, Dict]) -> Tuple[Tuple, Dict]:
+        return (), {}
 
-@tool(args_schema=latest_movie)
-def get_latest_movies(query:str) -> pd.DataFrame:
+    def _run(self):
+        response = get_latest_movies()
+        return response
+
+    def _arun(self):
+        raise NotImplementedError("get_latest_movies does not support async")
+
+def get_latest_movies() -> dict:
     """
     Returns: 다음 영화에서 크롤링한 최신 영화 정보를 리턴한다.
     현재는 그냥 데이터프레임을 넘기지만 나중에 agent화 할 예정.
     """
-    
-    return pd.read_csv('./data/daum_movie/movie_list.csv')
+    return pd.read_csv('./data/daum_movie/movie_list.csv').head().to_dict()
